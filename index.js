a7c079f1c3ca1ff92710363005d8a08292648fac
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2020-08-08 15:56:31 +0200

    Add version4 module b.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moduleA = require('moduleA');
 
 function getValue () {
-  return 'module-b-master-v3' + ' ' + moduleA.getValue();
+  return 'module-b-master-v4' + ' ' + moduleA.getValue();
 }
 
 module.exports = {
