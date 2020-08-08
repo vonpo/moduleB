6afeb6e6d8f04c886e53a16839a5f0248039229d
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2020-08-08 15:54:00 +0200

    Fix module b.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moduleA = require('moduleA');
 
 function getValue () {
-  return 'module-b-master-v2' + ' ' + moduleA.getValue();
+  return 'module-b-master-v3' + ' ' + moduleA.getValue();
 }
 
 module.exports = {
